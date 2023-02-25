d64d11a898dd71efbbd25a6e60803f6b01f14834
Pilar Fernandez Nieto <rla10309@educastur.es>
2023-02-25 16:45:58 +0100

    Finalizados ejercicios 5 y 6

---
+++ Formularios_Eventos/js/ejercicio05.js
@@ -0,0 +1,16 @@
+var capa1 = document.getElementById("capa1");
+var capa2 = document.getElementById("capa2");
+var capa3 = document.getElementById("capa3");
+//BUBBLING
+capa1.addEventListener("click", function(){
+    alert("Has pulsado la capa 1");
+});
+
+
+capa2.addEventListener("click", function(){
+    alert("Has pulsado la capa 2");
+});
+capa3. addEventListener("click", function(){
+    alert("Has pulsado la capa 3");
+});
+
---
+++ Formularios_Eventos/js/ejercicio06.js
@@ -0,0 +1,13 @@
+var capa1 = document.getElementById("capa1");
+var capa2 = document.getElementById("capa2");
+var capa3 = document.getElementById("capa3");
+//CAPTURING
+capa1.addEventListener("click", function(){
+    alert("Has pulsado en la caja 1");
+}, true);
+capa2.addEventListener("click", function(){
+    alert("Has pulsado en la caja 2");
+}, true);
+capa3.addEventListener("click", function(){
+    alert("Has pulsado en la caja 3");
+}, true);
