f9ee13e375952879cd4cfafff817d8bb8a337edc
Pilar Fernández Nieto <rla10309@educastur.es>
2023-03-11 16:09:12 +0100

    Se consigue eliminar la botonera y el mensaje de error. Añadidos comentarios

--- TAREA_UT5/ejercicio01/js/botones.js
+++ TAREA_UT5/ejercicio01/js/botones.js
@@ -5,44 +5,61 @@
  */
 window.onload = ejercicioBotones;
 /**
- *
+ *ejercicioBotones
+ * función principal del ejercicio que salta al cargar la ventana
  */
 function ejercicioBotones() {
-  var boton = document.getElementById("boton");
+  /*Variable donde se almacena el botón que va a escuchar el evento*/
+  var boton = document.getElementById("boton"); 
+  /*Variable que almacena la etiqueta donde se va a generar la botonera*/
   var contenedor = document.getElementById("botones");
-  var error = document.getElementById("informacion");
+  /*Variable que almacena la etiqueta  donde se encuentra el botón y donde se va a generar el mensaje de error, si lo hubiera*/
+  var primerDiv = document.getElementById("informacion");
   var n = 1;
   var btn = "";
   var vueltas = 0;
 
+  
   boton.addEventListener("click", generaBotones);
 
+  
+  /**
+   * generaBotones
+   * funcion que se ejecutará al pulsar el botón que genera la botonera
+   */
   function generaBotones() {
+    /*variable para la dimensión de la botonera*/
     var tamaño = document.getElementById("dimension").value;
+    /*variable para el input que recoge la dimensión*/
     var entrada = document.getElementById("dimension");
+    /*Colección donde se almacenan todos los botonesd que se generen*/
     var b = contenedor.getElementsByClassName("boton");
+    /*Párrafo para mostrar el mensaje de error, si lo hubiera*/
+    var alerta = document.createElement("p");
 
+    /*creamos un evento para eliminar la botonera creada, en caso de que la hubiera*/
     entrada.addEventListener("click", function () {
       if (vueltas != 0) {
         var bt = b.length - 1;
-
         for (var i = 1; i <= tamaño; i++) {
           for (var j = 1; j <= tamaño; j++) {
-            contenedor.removeChild(b[bt]);
+            contenedor.removeChild(b[bt]); /*elimina los botones creados*/
             bt--;
           }
         }
         n = 1;
-        entrada.textContent = '';
+        entrada.textContent = "";
       }
     });
+    /*Condición para que siga adelante en caso de que los datos sean correctos o que muestra un mensaje en el caso de que no*/
     if (tamaño === "" || tamaño < 2) {
-      var alerta = document.createElement("p");
       alerta.setAttribute("class", "error");
-      alerta.textContent =
-        "Esa dimensión no es válida. Introduzca un número mayor de 1";
-      error.appendChild(alerta);
+      alerta.textContent = "Esa dimensión no es válida. Introduzca un número mayor de 1";
+      primerDiv.appendChild(alerta);
+      /*se añade un temporizador para que el mensaje de alerta desaparezca a los 3 segundos*/
+      const temporizador = setTimeout(() => primerDiv.removeChild(alerta), 3000);
     } else {
+      /*creamos la botonera*/
       for (var i = 1; i <= tamaño; i++) {
         for (var j = 1; j <= tamaño; j++) {
           btn = document.createElement("button");
@@ -55,22 +72,25 @@ function ejercicioBotones() {
         var salto = document.createElement("br");
         contenedor.appendChild(salto);
       }
-
       var btn_elegido;
       var cont = 0;
 
       for (var i = 0; i < b.length; i++) {
         b[i].addEventListener("click", getUbicacion);
       }
+
+      /**
+       * getUbitacion
+       * @param {Event} e 
+       * función que recoge la fila y columna del botón pulsado
+       */
       function getUbicacion(e) {
         btn_elegido = e.target.id;
         for (var f = 1; f <= tamaño; f++) {
           for (var c = 1; c <= tamaño; c++) {
             cont++;
             if (cont == btn_elegido)
-              alert(
-                `Has pulsado el botón situado el la fila ${f} columna ${c}`
-              );
+              alert(`Has pulsado el botón situado el la fila ${f} columna ${c}`);
           }
         }
       }
