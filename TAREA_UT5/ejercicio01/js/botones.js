f0864d0434919d1b4d5c9a3b8dbb09e4abaef8aa
Pilar Fernández Nieto <rla10309@educastur.es>
2023-03-11 13:10:29 +0100

    Arreglado problema de generación de botones

--- TAREA_UT5/ejercicio01/js/botones.js
+++ TAREA_UT5/ejercicio01/js/botones.js
@@ -4,10 +4,8 @@
  * @description Creación de una botonera flexible a la que pasaremos por pantalla el formato. Al puslar uno de los botones deberá devolver la fila y la columna del botón pulsado
  */
 window.onload = ejercicioBotones;
-
-
 /**
- * 
+ *
  */
 function ejercicioBotones() {
   var boton = document.getElementById("boton");
@@ -15,10 +13,29 @@ function ejercicioBotones() {
   var error = document.getElementById("informacion");
   var n = 1;
   var btn = "";
+  var vueltas = 0;
+
   boton.addEventListener("click", generaBotones);
 
   function generaBotones() {
     var tamaño = document.getElementById("dimension").value;
+    var entrada = document.getElementById("dimension");
+    var b = contenedor.getElementsByClassName("boton");
+
+    entrada.addEventListener("click", function () {
+      if (vueltas != 0) {
+        var bt = b.length - 1;
+
+        for (var i = 1; i <= tamaño; i++) {
+          for (var j = 1; j <= tamaño; j++) {
+            contenedor.removeChild(b[bt]);
+            bt--;
+          }
+        }
+        n = 1;
+        entrada.textContent = '';
+      }
+    });
     if (tamaño === "" || tamaño < 2) {
       var alerta = document.createElement("p");
       alerta.setAttribute("class", "error");
@@ -38,23 +55,26 @@ function ejercicioBotones() {
         var salto = document.createElement("br");
         contenedor.appendChild(salto);
       }
-      var b = contenedor.getElementsByClassName("boton");
+
       var btn_elegido;
       var cont = 0;
+
       for (var i = 0; i < b.length; i++) {
         b[i].addEventListener("click", getUbicacion);
       }
       function getUbicacion(e) {
-          btn_elegido = e.target.id;
-          for (var f = 1; f <= tamaño; f++) {
-            for (var c = 1; c <= tamaño; c++) {
-              cont++;
-              if (cont == btn_elegido) 
-                alert(`Has pulsado el botón situado el la fila ${f} columna ${c}`); 
-            }
+        btn_elegido = e.target.id;
+        for (var f = 1; f <= tamaño; f++) {
+          for (var c = 1; c <= tamaño; c++) {
+            cont++;
+            if (cont == btn_elegido)
+              alert(
+                `Has pulsado el botón situado el la fila ${f} columna ${c}`
+              );
           }
         }
       }
     }
+    vueltas++;
   }
-
+}
