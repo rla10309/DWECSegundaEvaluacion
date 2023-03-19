6c733b1994b2bfa38495f5491986f3fb9a75ec3f
Pilar Fernández Nieto <rla10309@educastur.es>
2023-03-19 10:25:36 +0100

    Entraga final TAREAUT5

--- TAREA_UT5/ejercicio01/js/botones.js
+++ TAREA_UT5/ejercicio01/js/botones.js
@@ -3,17 +3,15 @@
  * @version 1.0
  * @description Creación de una botonera flexible a la que pasaremos por pantalla el formato. Al puslar uno de los botones deberá devolver la fila y la columna del botón pulsado
  */
-window.onload = ejercicioBotones;
+window.onload = inicia;
 /**
- *ejercicioBotones
+ * inicia
  * función principal del ejercicio que salta al cargar la ventana
  */
-function ejercicioBotones() {
-  /*Variable donde se almacena el botón que va a escuchar el evento*/
+function inicia() {
+  /* DECLARACIÓN DE VARIABLES */
   var boton = document.getElementById("boton"); 
-  /*Variable que almacena la etiqueta donde se va a generar la botonera*/
   var contenedor = document.getElementById("botones");
-  /*Variable que almacena la etiqueta  donde se encuentra el botón y donde se va a generar el mensaje de error, si lo hubiera*/
   var primerDiv = document.getElementById("informacion");
   var n = 1;
   var btn = "";
@@ -21,28 +19,24 @@ function ejercicioBotones() {
 
   
   boton.addEventListener("click", generaBotones);
-
-
   
   /**
    * generaBotones
    * funcion que se ejecutará al pulsar el botón que genera la botonera
    */
   function generaBotones() {
-    /*variable para la dimensión de la botonera*/
-    var tamaño = document.getElementById("dimension").value;
-    /*variable para el input que recoge la dimensión*/
+    /* VARIABLES LOCALES */
+    /* Recoge el valor introducido*/
+    var tamaño = document.getElementById("dimension").value; 
+    /*Recoge el elemento del DOM sobre el que se va a operar*/
     var entrada = document.getElementById("dimension");
     /*Colección donde se almacenan todos los botonesd que se generen*/
     var b = contenedor.getElementsByClassName("boton");
     /*Párrafo para mostrar el mensaje de error, si lo hubiera*/
     var alerta = document.createElement("p");
-   
-
-
 
     /*creamos un evento para eliminar la botonera creada, en caso de que la hubiera*/
-    entrada.addEventListener("click", function (e) {
+    entrada.addEventListener("click", function () {
       var saltos = contenedor.querySelectorAll("br");
       var elementos = contenedor.querySelectorAll(".boton");
       entrada.value = '';
@@ -58,7 +52,7 @@ function ejercicioBotones() {
       alerta.textContent = "Esa dimensión no es válida. Introduzca un número mayor de 1";
       primerDiv.appendChild(alerta);
       /*se añade un temporizador para que el mensaje de alerta desaparezca a los 3 segundos*/
-      const temporizador = setTimeout(() => primerDiv.removeChild(alerta), 3000);
+      setTimeout(() => primerDiv.removeChild(alerta), 3000);
     } else {
       /*creamos la botonera*/
       for (var i = 1; i <= tamaño; i++) {
@@ -73,13 +67,14 @@ function ejercicioBotones() {
         var salto = document.createElement("br");
         contenedor.appendChild(salto);
       }
-      var btn_elegido;
-      var cont = 0;
+    
 
+      /*bucle para asignar eventos a cada botón generado*/
       for (var i = 0; i < b.length; i++) {
         b[i].addEventListener("click", getUbicacion);
       }
-
+      var btn_elegido;
+      var cont = 0;
       /**
        * getUbitacion
        * @param {Event} e 
--- TAREA_UT5/ejercicio02/js/solucionPanel.js
+++ TAREA_UT5/ejercicio02/js/solucionPanel.js
@@ -1,4 +1,16 @@
+/**
+ * @author Pilar Fernández Nieto
+ * @version 1.0
+ * @date 19/03/2023
+ * @description aplicación que simule un panel de dibujo
+ */
+
+
+/**
+ * función principal que se ejecuta al cargar la página
+ */
 window.onload = function () {
+  /* VARIABLES */
   var btn_amarillo = document.getElementById("amarillo");
   var btn_verde = document.getElementById("verde");
   var btn_negro = document.getElementById("negro");
@@ -8,6 +20,7 @@ window.onload = function () {
   var estado_pincel = document.getElementById("estado");
   var panel = document.getElementById("panel");
 
+  /*Al hacer click en alguno de los colores se activa la función de pintar*/
   btn_amarillo.addEventListener("click", usaColor);
   btn_verde.addEventListener("click", usaColor);
   btn_negro.addEventListener("click", usaColor);
@@ -15,14 +28,30 @@ window.onload = function () {
   btn_azul.addEventListener("click", usaColor);
   btn_blanco.addEventListener("click", usaColor);
 
+  
+  /**
+   * usaColor 
+   * @param {Event} e 
+   * función que activa el panel de dibujo
+   */
   function usaColor(e) {
+    /*Se carga el id del color elegido*/
     var color_elegido = e.target.id;
-    estado_pincel.textContent = "Pincel Activado.Color " + color_elegido;
+    /*Array con los elementos vacíos de la cuadricula que sirve de panel*/
     var c = panel.getElementsByClassName("cuadricula");
+    estado_pincel.textContent = "Pincel Activado.Color " + color_elegido;
+    estado_pincel.addEventListener("click", desactiva);
+    
+    /*Con este bucle se asigna el evento y manejador para cada cuadricula del panel*/
     for (var i = 0; i < c.length; i++) {
       var cuadrado = c[i];
       cuadrado.addEventListener("click", pinta);
     }
+
+    /**
+     * pinta
+     * función que cambia el color de fondo de la cuadrícla con el color elegido
+     */
     function pinta() {
       switch (color_elegido) {
         case "amarillo":
@@ -42,7 +71,21 @@ window.onload = function () {
           break;
         case "blanco":
           this.style.background = "white";
+          break;
       }
     }
+
+    /**
+     * desactiva
+     * al hacer click en el panel informativo se desactiva la función de pintar
+     */
+    function desactiva(){
+      estado_pincel.innerHTML = "Pincel desactivado";
+      for (var i = 0; i < c.length; i++) {
+        var cuadrado = c[i];
+        cuadrado.removeEventListener("click", pinta);
+      }
+      
   }
-};
+}
+}
