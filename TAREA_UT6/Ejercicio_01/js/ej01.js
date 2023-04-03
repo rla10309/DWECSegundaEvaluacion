5953c2809ada027073af08c876f9bbe857c77114
Pilar Fernández Nieto <rla10309@educastur.es>
2023-04-03 14:45:09 +0200

    Ejercicios jQuery y comienzo TareaUT6

--- TAREA_UT6/Ejercicio_01/js/ej01.js
+++ TAREA_UT6/Ejercicio_01/js/ej01.js
@@ -1,26 +1,48 @@
 
-window.onload = function(){
+window.onload = function () {
 
-   
+
     let add_btn = document.getElementById("add");
-    
-    
-    
-    add_btn.addEventListener("click",gestionSelect);
-}
-
-    
-
-function gestionSelect(e){
-    console.log(e);
-    e.preventDefault();
+    let enviar_btn = document.getElementById("enviar");
     let lista = [];
-    let indice = document.formulario.addaficiones.selectedIndex;
-    let opcionAdd = document.formulario.addaficiones.options[indice];
-    let opcionRem = document.formulario.eliminaaficiones.options[indice];
-    lista.push(opcionRem.text);
-    opcionAdd.classList.add("no_display");
-    opcionRem.classList.remove("no_display");
-    console.log(lista);
+    
+    add_btn.addEventListener("click", gestionSelect);
+    
+       
+    }
+    
+
+
+
+
+function gestionSelect(e) {
+    e.preventDefault();
    
+    let selectAdd = document.getElementById("addaficiones");
+    let selectRem = document.getElementById("eliminaaficiones");
+    
+
+    for (var i=0;i<selectAdd.options.length;i++){
+        if(selectAdd.options[i].selected === true){
+            lista.push(selectAdd.options[i].text);
+            selectAdd.options[i].classList.add("no_display");
+            selectRem.options[i].classList.remove("no_display");
+        }
+        
+    }
+    console.log(lista);
+    // let indice = document.formulario.addaficiones.selectedIndex;
+    // let opcionAdd = document.formulario.addaficiones.options[indice];
+    // let opcionRem = document.formulario.eliminaaficiones.options[indice];
+    // lista.push(opcionRem.text);
+    // opcionAdd.classList.add("no_display");
+    // opcionRem.classList.remove("no_display");
+    // console.log(lista);
+
 }
+// var arreglo = [];
+// var selectObject = document.getElementById("secretariaTxt");
+// for (var i = 0; i < selectObject.options.length; i++) {
+//     if (selectObject.options[i].selected === true) {
+//         arreglo.push(selectObject.options[i].value);
+//         selectObject.options[i].value + '">' + selectObject.options[i].value + '</option>');
