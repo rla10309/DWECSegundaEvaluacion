f41ad0919e04b02a67478adffbb0e8c44e67f104
Pilar Fernandez Nieto <rla10309@educastur.es>
2023-04-04 15:54:57 +0200

    Terminado ejercicio05 hojaUT6Jquery

--- CuadernoEjerciciosUT6_JQuery/ejercicio05/js/ej05.js
+++ CuadernoEjerciciosUT6_JQuery/ejercicio05/js/ej05.js
@@ -1,42 +1,39 @@
 $(document).ready(function () {
-
-    //Funcionalidad de Editar
-    $(".editar").each(function(i){
-        $(this).click(function(e){
-            $("input.precio").eq(i).removeAttr("readonly");
-             
-            });
-        });
-  
-    $("input.precio").keypress(function (e) { 
-        if(e.code == "Enter" || e.code =="NumpadEnter")
-            $(this).attr("readonly", "true");
+  //Funcionalidad de Editar
+  $(".editar").each(function (i) {
+    $(this).click(function (e) {
+      $("input.precio").eq(i).removeAttr("readonly");
     });
-    //Ocultar
+  });
 
-    
-$(".ocultar").each(function(i){
-    $(this).click(function (e){
-        $("tbody tr td:nth-child(1)").eq(i).css("visibility", "hidden");
-        $("tbody tr td:nth-child(2)").eq(i).css("visibility", "hidden");
-    })
-     
-  });
- $(".mostrar").each(function(i){
-    $(this).click(function (e){
-        $("tbody tr td:nth-child(1)").eq(i).css("visibility", "visible");
-        $("tbody tr td:nth-child(2)").eq(i).css("visibility", "visible");
-    })
-     
-  });
-    
-  $(".eliminar").each(function(i){
-    $(this).click(function(e){
-        $("tbody tr").eq(i).hide();
-    })
+  //retornamos el atributo readonly al presionar Enter
+  $("input.precio").keypress(function (e) {
+    if (e.code == "Enter" || e.code == "NumpadEnter")
+      $(this).attr("readonly", "true");
   });
 
 
+  //ocultar producto y precio
+  $(".ocultar").each(function (i) {
+    $(this).click(function (e) {
+      $("tbody tr td:nth-child(1)").eq(i).css("visibility", "hidden");
+      $("tbody tr td:nth-child(2)").eq(i).css("visibility", "hidden");
+    });
+  });
+
+  //mostrar producto y precio
+  $(".mostrar").each(function (i) {
+    $(this).click(function (e) {
+      $("tbody tr td:nth-child(1)").eq(i).css("visibility", "visible");
+      $("tbody tr td:nth-child(2)").eq(i).css("visibility", "visible");
+    });
+  });
 
 
+  //eliminar la fila entera
+  $(".eliminar").each(function (i) {
+    $(this).click(function (e) {
+      $("tbody tr").eq(i).hide();
+    });
+  });
 });
