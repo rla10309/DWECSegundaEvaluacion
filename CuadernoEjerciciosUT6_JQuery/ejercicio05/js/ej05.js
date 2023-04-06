414584158736452353e5a6cad07054fb3d8b5c67
Pilar Fernández Nieto <rla10309@educastur.es>
2023-04-06 15:01:32 +0200

    Inicio Ejercicio6 cuaderno jQuery

--- CuadernoEjerciciosUT6_JQuery/ejercicio05/js/ej05.js
+++ CuadernoEjerciciosUT6_JQuery/ejercicio05/js/ej05.js
@@ -1,39 +1,39 @@
 $(document).ready(function () {
-  //Funcionalidad de Editar
-  $(".editar").each(function (i) {
-    $(this).click(function (e) {
-      $("input.precio").eq(i).removeAttr("readonly");
+    //Funcionalidad de Editar
+    $(".editar").each(function (i) {
+      $(this).click(function (e) {
+        $("input.precio").eq(i).removeAttr("readonly");
+      });
+    });
+  
+    //retornamos el atributo readonly al presionar Enter
+    $("input.precio").keypress(function (e) {
+      if (e.code == "Enter" || e.code == "NumpadEnter")
+        $(this).attr("readonly", "true");
+    });
+  
+  
+    //ocultar producto y precio
+    $(".ocultar").each(function (i) {
+      $(this).click(function (e) {
+        $("tbody tr td:nth-child(1)").eq(i).css("visibility", "hidden");
+        $("tbody tr td:nth-child(2)").eq(i).css("visibility", "hidden");
+      });
+    });
+  
+    //mostrar producto y precio
+    $(".mostrar").each(function (i) {
+      $(this).click(function (e) {
+        $("tbody tr td:nth-child(1)").eq(i).css("visibility", "visible");
+        $("tbody tr td:nth-child(2)").eq(i).css("visibility", "visible");
+      });
+    });
+  
+  
+    //eliminar la fila entera
+    $(".eliminar").each(function (i) {
+      $(this).click(function (e) {
+        $("tbody tr").eq(i).hide();
+      });
     });
   });
-
-  //retornamos el atributo readonly al presionar Enter
-  $("input.precio").keypress(function (e) {
-    if (e.code == "Enter" || e.code == "NumpadEnter")
-      $(this).attr("readonly", "true");
-  });
-
-
-  //ocultar producto y precio
-  $(".ocultar").each(function (i) {
-    $(this).click(function (e) {
-      $("tbody tr td:nth-child(1)").eq(i).css("visibility", "hidden");
-      $("tbody tr td:nth-child(2)").eq(i).css("visibility", "hidden");
-    });
-  });
-
-  //mostrar producto y precio
-  $(".mostrar").each(function (i) {
-    $(this).click(function (e) {
-      $("tbody tr td:nth-child(1)").eq(i).css("visibility", "visible");
-      $("tbody tr td:nth-child(2)").eq(i).css("visibility", "visible");
-    });
-  });
-
-
-  //eliminar la fila entera
-  $(".eliminar").each(function (i) {
-    $(this).click(function (e) {
-      $("tbody tr").eq(i).hide();
-    });
-  });
-});
